c097fe5b3b31e80cf8fcec7cbd743237d7a28ee2
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-03-17 14:17:44 +0700

    Update channelplay.py

--- DarenMusik/utils/channelplay.py
+++ DarenMusik/utils/channelplay.py
@@ -1,5 +1,5 @@
 from DarenMusik import app
-from.DarenMusik.utils.database import get_cmode
+from DarenMusik.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
