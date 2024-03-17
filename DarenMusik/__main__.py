5864aca262c33c64d575780b6b7b97c984b98f87
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-03-17 14:32:44 +0700

    Update __main__.py

--- DarenMusik/__main__.py
+++ DarenMusik/__main__.py
@@ -58,5 +58,5 @@ async def init():
     LOGGER("DarenMusik").info("Stopping Daren Musik Bot...")
 
 
-if name == "main":
+if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
